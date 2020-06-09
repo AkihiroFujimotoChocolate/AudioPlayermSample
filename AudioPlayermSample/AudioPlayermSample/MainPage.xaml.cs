49e2187be502f13014c7e63ec40c2865c808a7de
Akihiro Fujimoto <akihiro.dvrp01@gmail.com>
2020-06-09 23:28:55 +0900

    the first commit

---
+++ AudioPlayermSample/AudioPlayermSample/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using AudioPlayermSample.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AudioPlayermSample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public IList<AudioItem> AudioItems { get; private set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            AudioItems = new List<AudioItem>();
+            AudioItems.Add(new AudioItem
+            {
+                Title = "SampleA",
+                Description = "This is Audio Sample A",
+                Source = "C:\\Users\\akihi\\source\\testdata\\audio\\sampleA.wma"
+            });
+            AudioItems.Add(new AudioItem
+            {
+                Title = "SampleB",
+                Description = "This is Audio Sample B",
+                Source = "C:\\Users\\akihi\\source\\testdata\\audio\\sampleB.wma"
+            });
+            AudioItems.Add(new AudioItem
+            {
+                Title = "SampleC",
+                Description = "This is Audio Sample C",
+                Source = "C:\\Users\\akihi\\source\\testdata\\audio\\sampleC.wma"
+            });
+
+            BindingContext = this;
+        }
+
+        void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+            AudioItem selectedItem = e.SelectedItem as AudioItem;
+            LabelTitile.Text = selectedItem.Title;
+            LabelDescription.Text = selectedItem.Description;
+        }
+        void OnListViewItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            AudioItem tappedItem = e.Item as AudioItem;
+        }
+    }
+}
---
+++ AudioPlayermSample/AudioPlayermSample/Models/AudioItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace AudioPlayermSample.Models
+{
+    public class AudioItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Source { get; set; }
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
